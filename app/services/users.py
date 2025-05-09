a1c7774008d20fc3f80feb48f3f0b83b838dad91
kieran <kieran.forbes.2022@uni.strath.ac.uk>
2025-05-09 11:36:42 +0100

    Formatted user database into project structure

--- app/services/users.py
+++ app/services/users.py
@@ -1,37 +1,63 @@
-from typing import ClassVar, Tuple
-from typing import Optional
+from typing import ClassVar, Tuple, Optional
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
-from pydantic import Field, StrictStr
-from typing_extensions import Annotated
-
-from app.models.user import User
+from app.models.user       import User        as PydanticUser
 from app.models.user_input import UserInput
-
+from app.database.tables.user  import UserTable
 
 class UsersService:
     subclasses: ClassVar[Tuple] = ()
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        UsersService.subclasses = UsersService.subclasses + (cls,)
+        UsersService.subclasses += (cls,)
 
-    async def create_user(self, user_input: Annotated[
-        Optional[UserInput], Field(description="User object creation and update in store")], ) -> User:
-        """This can only be done by the superuser."""
-        ...
+    async def create_user(self, user_input: UserInput, db: AsyncSession) -> PydanticUser:
+        raise NotImplementedError
 
-    async def delete_user(self, username: Annotated[
-        StrictStr, Field(description="The username that needs to be deleted")], ) -> None:
-        """This can only be done by the logged in user."""
-        ...
+    async def get_user_by_name(self, username: str, db: AsyncSession) -> PydanticUser:
+        raise NotImplementedError
 
-    async def get_user_by_name(self, username: Annotated[
-        StrictStr, Field(description="The name that needs to be fetched. Use users1 for testing")], ) -> User:
-        """Get uses detail based on username."""
-        ...
+    async def update_user(self, username: str, user_input: UserInput, db: AsyncSession) -> None:
+        raise NotImplementedError
 
-    async def update_user(self, username: Annotated[StrictStr, Field(description="name that need to be deleted")],
-                          user_input: Annotated[Optional[UserInput], Field(
-                              description="User object creation and update in store")], ) -> None:
-        """This can only be done by the logged in user or superuser."""
-        ...
+    async def delete_user(self, username: str, db: AsyncSession) -> None:
+        raise NotImplementedError
+
+class SqliteUsersService(UsersService):
+    """Concrete implementation against UserTable (SQLite or MySQL)."""
+
+    async def create_user(self, user_input: UserInput, db: AsyncSession) -> PydanticUser:
+        data = user_input.model_dump(exclude_none=True)
+        db_obj = UserTable(**data)
+        db.add(db_obj)
+        await db.commit()
+        await db.refresh(db_obj)
+        return PydanticUser.model_validate(db_obj)
+
+    async def get_user_by_name(self, username: str, db: AsyncSession) -> PydanticUser:
+        result = await db.execute(select(UserTable).filter_by(username=username))
+        db_obj = result.scalars().first()
+        if not db_obj:
+            raise HTTPException(status_code=404, detail="User not found")
+        return PydanticUser.model_validate(db_obj)
+
+    async def update_user(self, username: str, user_input: UserInput, db: AsyncSession) -> None:
+        result = await db.execute(select(UserTable).filter_by(username=username))
+        db_obj = result.scalars().first()
+        if not db_obj:
+            raise HTTPException(status_code=404, detail="User not found")
+        updates = user_input.model_dump(exclude_none=True)
+        for field, value in updates.items():
+            setattr(db_obj, field, value)
+        await db.commit()
+
+    async def delete_user(self, username: str, db: AsyncSession) -> None:
+        result = await db.execute(select(UserTable).filter_by(username=username))
+        db_obj = result.scalars().first()
+        if not db_obj:
+            raise HTTPException(status_code=404, detail="User not found")
+        await db.delete(db_obj)
+        await db.commit()
